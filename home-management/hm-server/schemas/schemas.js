1503d0e8fc14057d8055f7c14ac2f111e3fc8f3d
Shahriar <fem.nwu@gmail.com>
2022-05-22 18:34:45 +0600

    schema updated

--- home-management/hm-server/schemas/schemas.js
+++ home-management/hm-server/schemas/schemas.js
@@ -1,17 +1,5 @@
 const mongoose = require("mongoose");
 
-const memberSchema = new mongoose.Schema({
-  name: String,
-  mobile: String,
-  meals: [
-    {
-      date: { type: Date, default: Date.now },
-      mealCount: Number,
-      bazarCost: Number,
-    },
-  ],
-});
-
 const userSchema = new mongoose.Schema(
   {
     name: String,
@@ -24,25 +12,53 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+const memberSchema = new mongoose.Schema({
+  name: String,
+  mobile: String,
+  meals: [
+    {
+      mealCount: { type: Number, default: 0 },
+      bazarCost: Number,
+      date: { type: Date, default: Date.now },
+    },
+  ],
+});
+
 const utilitySchema = new mongoose.Schema(
   {
     currentBill: Number,
     khalarBill: Number,
     internetBill: Number,
     houseRent: Number,
-    otherBill: Number,
+    otherBills: [
+      {
+        title: String,
+        expenditure: Number,
+      },
+    ],
     date: { type: Date, default: Date.now },
   },
   { timestamp: true }
 );
 
-// const activityLogSchema = new mongoose.Schema({
-
-// })
+const activityLogSchema = new mongoose.Schema(
+  {
+    name: String,
+    mobile: String,
+    activity: String,
+    date: { type: Date, default: Date.now },
+  },
+  { timestamp: true }
+);
 
 const Member = mongoose.model("Member", memberSchema);
 const User = mongoose.model("User", userSchema);
+const Utility = mongoose.model("Utility", utilitySchema);
+const ActivityLog = mongoose.model("ActivityLog", activityLogSchema);
+
 module.exports = {
   Member,
   User,
+  Utility,
+  ActivityLog,
 };
