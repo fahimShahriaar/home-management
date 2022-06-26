fbca7c99c8fa4c6a727169caed8edf20e7e59ba6
Shahriar <fem.nwu@gmail.com>
2022-06-26 22:28:49 +0600

    react-native navigation initialized

--- home-management/hm-client/App.js
+++ home-management/hm-client/App.js
@@ -1,24 +1,45 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StyleSheet, View } from "react-native";
-import FlexBox from "./Components/FlexBox";
-import PracticeOne from "./Components/PracticeOne";
-import PracticeTwo from "./Components/PracticeTwo";
+// import FlexBox from "./Components/FlexBox";
+// import PracticeOne from "./Components/PracticeOne";
+// import PracticeTwo from "./Components/PracticeTwo";
 import Signup from "./Components/Signup";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.appContainer}>
-      {/* <PracticeOne /> */}
-      {/* <PracticeTwo /> */}
-      {/* <FlexBox /> */}
-      <Signup />
-    </View>
+    <NavigationContainer>
+      <View style={styles.appContainer}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Signup"
+            component={Signup}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    paddingVertical: 50,
-    paddingHorizontal: 10,
+    // paddingVertical: 50,
+    // paddingHorizontal: 10,
     flex: 1,
   },
 });
+
+{
+  /* <PracticeOne /> */
+}
+{
+  /* <PracticeTwo /> */
+}
+{
+  /* <FlexBox /> */
+}
+{
+  /* <Signup /> */
+}
